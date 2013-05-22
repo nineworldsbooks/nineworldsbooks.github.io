4b433405794a72f968b167d64bbfeb15cacecae9
James Nutt <james.srn@gmail.com>
2013-05-22 09:59:43 +0100

    Mix and match wounds!

--- woundomatic/wounds.js
+++ woundomatic/wounds.js
@@ -38,7 +38,33 @@ var stabbingTarget = Array(
 // Crushing
 var crushingWounds = Array(
 	'cracked',
-	'broken'
+	'broken',
+	'splintered'
+	);
+
+var crushingTarget = Array(
+	'side of skull',
+	'side of jaw',
+	'collar bone',
+	'shoulder blade',
+	'side of ribcage',
+	'femur'
+	);
+
+// Slashing
+var slashingWounds = Array(
+	'cut',
+	'slashed',
+	'severed'
+	);
+
+var slashingTarget = Array(
+	'fingers',
+	'hand',
+	'forearm',
+	'upper arm',
+	'side of neck',
+	'thigh'
 	);
 
 var sides = Array(
@@ -49,29 +75,79 @@ var sides = Array(
 
 function roll()
 {
-	var type = document.getElementById("weapon-type").value;
+	var results = getFistWounds();
 
+	results += getStabbingWounds();
+
+	results += getCrushingWounds();
+
+	results += getSlashingWounds();
+	
+	document.getElementById("results").innerHTML = results;
+}
+
+function getFistWounds()
+{
+	var results = "";
 	var area, injury;
 
-	switch(type)
-	{
-		case 'fists':
-			injury = punchingWounds[ dN( punchingWounds.length ) ];
-			area = getSide() + " " + punchingTarget[ dN( punchingTarget.length ) ];
-			break;
-		case 'stabbing':
-			injury = stabbingWounds[ dN( stabbingWounds.length ) ];
-			area = getSide() + " " + stabbingTarget[ dN( stabbingTarget.length ) ];
-			break;
-		case 'crushing':
-			break;
-		case 'slashing':
-			break;
-	}
+	var fistwounds = document.getElementById("fists").value;
+	results += '<b>Fists:</b><br>';
+	for ( var i = 0; i < fistwounds; i++ ) {
+		injury = punchingWounds[ dN( punchingWounds.length ) ];
+		area = getSide() + " " + punchingTarget[ dN( punchingTarget.length ) ];
+		results += injury + " " + area + '<br>';
+	};
 
-	var result = injury + " " + area;
+	return results;
+}
 
-	document.getElementById("results").innerHTML = result;
+function getStabbingWounds()
+{
+	var results = "";
+	var area, injury;
+
+	var stabbingwounds = document.getElementById("stabbing").value;
+	results += '<br><b>Stabbing:</b><br>';
+	for ( var i = 0; i < stabbingwounds; i++ ) {
+		injury = stabbingWounds[ dN( stabbingWounds.length ) ];
+		area = getSide() + " " + stabbingTarget[ dN( stabbingTarget.length ) ];
+		results += injury + " " + area + '<br>';
+	};
+
+	return results;
+}
+
+function getCrushingWounds()
+{
+	var results = "";
+	var area, injury;
+
+	var crushingwounds = document.getElementById("crushing").value;
+	results += '<br><b>Crushing:</b><br>';
+	for ( var i = 0; i < crushingwounds; i++ ) {
+		injury = crushingWounds[ dN( crushingWounds.length ) ];
+		area = getSide() + " " + crushingTarget[ dN( crushingTarget.length ) ];
+		results += injury + " " + area + '<br>';
+	};
+
+	return results;
+}
+
+function getSlashingWounds()
+{
+	var results = "";
+	var area, injury;
+
+	var slashingwounds = document.getElementById("slashing").value;
+	results += '<br><b>Slashing:</b><br>';
+	for ( var i = 0; i < slashingwounds; i++ ) {
+		injury = slashingWounds[ dN( slashingWounds.length ) ];
+		area = getSide() + " " + slashingTarget[ dN( slashingTarget.length ) ];
+		results += injury + " " + area + '<br>';
+	};
+
+	return results;
 }
 
 function getSide(){
